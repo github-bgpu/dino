f810065a048dd6682fc5d2938b9399c636798d48
Your Name <you@example.com>
2023-03-02 13:15:40 +0900

    add: loop

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const dino = document.querySelector("#dino")
+const fps = 60;
+
+const dino = document.querySelector("#dino");
+
+let y = 100;
+
+setInterval(() => {
+    console.log("!");
+}, 1000 / fps)
 
 dino.style.bottom = "100px";
