9409a9f062eabefd130b5a632a4d452f8f751981
Your Name <you@example.com>
2023-03-02 14:05:41 +0900

    add: cactus movement

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const G = 10;
 const DT = 1 / FPS;
 
 const dino = document.querySelector("#dino");
+const cactus = document.querySelector("#cactus");
 
 let y = 1500;
 let dy = 0;
 
+let x = 3500
+const dx = -10;
+
 setInterval(() => {
     dy = dy - G * DT
-
     y = y + dy
 
     if (y < 0) {
@@ -17,6 +20,14 @@ setInterval(() => {
     }
 
     dino.style.bottom = `${y * 0.25}px`
+
+    x = x + dx
+
+    if (x < -300) {
+        x = 3500
+    }
+
+    cactus.style.left = `${x * 0.25}px`
 }, DT)
 
 document.addEventListener("keydown", () => {
