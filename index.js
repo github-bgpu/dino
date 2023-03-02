0948015e8a0be63293515e7a2304cecf8e6bf24c
Your Name <you@example.com>
2023-03-02 13:40:09 +0900

    add: acceleration

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-const fps = 60;
+const FPS = 60;
+const G = 10;
+const DT = 1 / FPS;
 
 const dino = document.querySelector("#dino");
 
-let y = 100;
-let dy = 1;
+let y = 1500;
+let dy = 0;
 
 setInterval(() => {
+    dy = dy + G * DT
+
     y = y - dy
 
     if (y < 0) {
         y = 0
     }
 
-    dino.style.bottom = `${y}px`
-}, 1000 / fps)
+    dino.style.bottom = `${y * 0.25}px`
+}, DT)
 
 dino.style.bottom = "100px";
