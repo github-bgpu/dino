292125f58a9b2ca78532e093ab952191f3e87cce
Your Name <you@example.com>
2023-03-02 13:19:52 +0900

    add: gravity

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const fps = 60;
 const dino = document.querySelector("#dino");
 
 let y = 100;
+let dy = 1;
 
 setInterval(() => {
-    console.log("!");
+    y = y - dy
+
+    if (y < 0) {
+        y = 0
+    }
+
+    dino.style.bottom = `${y}px`
 }, 1000 / fps)
 
 dino.style.bottom = "100px";
